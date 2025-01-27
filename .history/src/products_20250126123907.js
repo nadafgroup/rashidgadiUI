2ebbb22721d4b0bd01e2c4b02898f42e0a51501c
nadafgroup <rashidgadimakers@gmail.com>
2025-01-27 15:46:18 +0530

    new update

---
+++ .history/src/products_20250126123907.js
@@ -0,0 +1,5 @@
+// Toggle the mobile menu when the hamburger icon is clicked
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+});
